d3b9cec91a96237ec3f487fa25074f0ab84119ba
tsaiyuyes7 <39110981+tsaiyuyes7@users.noreply.github.com>
2018-09-26 11:33:28 +0800

    Add files via upload

---
+++ wk3.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h" 
+//#include "images.h"
+SSD1306Wire  display(0x3c, 21, 22);
+void setup() {
+  Serial.begin(115200);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+
+}
+
+void loop() {
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, String(analogRead(A0)));
+  display.display();
+  delay(200);
+}
